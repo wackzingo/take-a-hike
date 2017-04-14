269932cf1650093d8b119af10565cb7e5e9f92c3
U-KEITHSLAPTOP\Keith <keithstellyes@github.com>
2017-04-14 16:35:36 -0700

    Initial constants

---
+++ tkh-constants.js
@@ -0,0 +1,5 @@
+const LOCATION_TRAITS = ["BASEBALL INFIELD", "COMMUNITY GARDEN POINT",
+                         "PICNIC SHELTER", "BIKE RACK", "TENNIS COURT",
+                         "DOG BAG", "BASKETBALL", "TRAIL PATH",
+                         "EXERCISE", "OFF LEASH AREA", "SPRAYGROUND",
+                         "PLAYGROUND", "SKATE AREA"]
